51a0e7e4f57498dd136d6196d951d67a93473daa
bowenjos <bowenjos@oregonstate.edu>
2017-06-03 14:23:38 -0700

    Assignment Two

---
+++ HW2/MinimaxPlayer.h
@@ -0,0 +1,66 @@
+/*
+ * MinimaxPlayer.h
+ *
+ *  Created on: Apr 17, 2015
+ *      Author: wong
+ */
+
+#ifndef MINIMAXPLAYER_H
+#define MINIMAXPLAYER_H
+
+#include "OthelloBoard.h"
+#include "Player.h"
+#include <vector>
+
+struct mima{
+	int c;
+	int r;
+	int val;
+};
+
+/**
+ * This class represents an AI player that uses the Minimax algorithm to play the game
+ * intelligently.
+ */
+class MinimaxPlayer : public Player {
+public:
+
+	/**
+	 * @param symb This is the symbol for the minimax player's pieces
+	 */
+	MinimaxPlayer(char symb);
+
+	/**
+	 * Destructor
+	 */
+	virtual ~MinimaxPlayer();
+
+	char get_other_symbol(char symbol);
+
+	int utility(OthelloBoard* cb);	
+
+	int max(OthelloBoard* cb, int r, int c);
+
+	int min(OthelloBoard* cb, int r, int c);
+
+	mima firstMax(OthelloBoard* b);
+
+	/**
+	 * @param b The board object for the current state of the board
+	 * @param col Holds the return value for the column of the move
+	 * @param row Holds the return value for the row of the move
+	 */
+    void get_move(OthelloBoard* b, int& col, int& row);
+
+    /**
+     * @return A copy of the MinimaxPlayer object
+     * This is a virtual copy constructor
+     */
+    MinimaxPlayer* clone();
+
+private:
+
+};
+
+
+#endif
